5e5dba39442791dd2e2bd5c2f0bdd13be5abbb2b
gabriel <ga-yo-so@hotmail.com>
2015-09-23 13:59:49 -0300

    #5 tests unitarios para SignUpHandler

--- server/tests/SignUpHandler_unittest.cpp
+++ server/tests/SignUpHandler_unittest.cpp
@@ -1,5 +1,19 @@
 
 #include "gtest/gtest.h"
+#include "SignUpHandler.h"
+
+TEST(SignUpHandlerTest, CrearSignUpHandlerNoFalla){
+    EXPECT_TRUE(new SignUpHandler());
+}
+
+/*TEST(SignUpHandlerTest, SignUpHandlerRegistraUnUsuario){
+
+}*/
+
+// por si queremos que no se acepten simbolos o algo en los nombres de usuario o passwords
+/*TEST(SignUpHandlerTest, SignUpHandlerNoAceptaNombresRaros){
+
+}*/
 
 int main(int argc, char **argv){
     testing::InitGoogleTest(&argc, argv);
