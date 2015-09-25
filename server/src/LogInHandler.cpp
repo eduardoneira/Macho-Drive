57bc487dd476230f8f96970bd50509b65f43e11e
gabriel <ga-yo-so@hotmail.com>
2015-09-25 13:26:52 -0300

    #5 actualizado log in handler, busca en la db el usuario y se fija que la contraseña indicada sea correcta. pero aun no genera token ni comunica el resultado del log in

--- server/src/LogInHandler.cpp
+++ server/src/LogInHandler.cpp
@@ -2,8 +2,9 @@
 #include <iostream>
 #include <string>
 #include "json/json.h"
+#include "User.h"
 
-LogInHandler::LogInHandler(){
+LogInHandler::LogInHandler(Database *db) : EventHandler(db) {
 
 }
 
@@ -11,18 +12,25 @@ LogInHandler::~LogInHandler(){
 
 }
 
-void LogInHandler::handle(struct http_message* hmsg){
-	using namespace Json;
+void LogInHandler::handle(HttpRequest &hmsg){
+	/*using namespace Json;
 	Reader reader;
-	Value value;	
+	Value value;
 	std::string body = "";
 	body.append(hmsg->body.p, hmsg->body.len);
-    if(reader.parse(/*builder,*/body.c_str(), value/*, false*/)){
+    if(reader.parse(body.c_str(), value)){
     	std::cout << value["user"] << std::endl;
     	std::cout << value["password"] << std::endl;
     	//dataBase.register(value["user"], value["password"]);
-	}
+	}*/
+    User user;
+    user.setUsername(hmsg.getCampo("username"));
+    //user.setPassword(hmsg.getCampo("password"));
 
+    std::cout << "pass antes: " << user.getValueToString() << std::endl;
+    Status s = this->db->get(user); // ver error en status
+    std::cout << "pass despues: " << user.getValueToString() << std::endl;
 
-
+    bool pass_match = (hmsg.getCampo("password").compare(user.getValueToString())) == 0;
+    std::cout << "match: " << pass_match << std::endl;
 }
