888034f5a4e5f0edc7691d044cbc44ecb72d497f
gabriel <ga-yo-so@hotmail.com>
2015-11-05 16:40:10 -0300

    Estuve comentando las clases DBElement y Database para documentar doxygen. Luego sigo con el resto

--- server/src/Database.h
+++ server/src/Database.h
@@ -7,19 +7,54 @@
 
 using namespace rocksdb;
 
+//!Clase abstracta como interface para base de datos.
+/*!Esta clase sirve como interface para las clases \
+DatabaseRocksDB y DatabaseMockRAM.
+*/
+
 class Database
 {
     public:
+
+        //!Funcion que inicializa la clase.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         Database() {}
         virtual ~Database() {}
 
+        //!Funcion que establece el path a la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status config(const std::string& db_path) = 0;
+
+        //!Funcion que abre la base de datos para ser leida o modificada.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status open() = 0;
+
+        //!Funcion que cierra la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual void close() = 0;
+
+        //!Funcion que elimina todos los elementos de la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status clear_all() = 0;
 
+        //!Funcion que elimina un elemento de la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status erase(DBElement &elem) = 0;
+
+        //!Funcion que agrega un elemento a la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status put(DBElement &elem) = 0;
+
+        //!Funcion que obtiene un elemento de la base de datos.
+        /*!Esta funcion es redefinida en las clases hijas.
+        */
         virtual Status get(DBElement &elem) = 0;
 
     protected:
