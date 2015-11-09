c43563ba2816a529c48b24352a640852d1cfc0d5
gabriel <ga-yo-so@hotmail.com>
2015-11-08 23:51:13 -0300

    closes #41 - closes #45 - tests de python automaticos (tambien se pueden correr como antes, imprimiendo todo en pantalla. Ver el archivo server_test_func.py)

--- server/run_tests.sh
+++ server/run_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for entry in build/tests/*; do
+for entry in build/unit_tests/*; do
   if [ -x "$entry" ]; then #si tiene permiso de ejecucion (archivos de texto tmb tienen, queria hacer que solo agarre ejecutables pero no se como. igual, en esta carpeta solo van a haber ejecutables asi que hasta se podria borrar el if
     ./$entry
   fi
