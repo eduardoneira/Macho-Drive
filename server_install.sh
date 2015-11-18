64f36c3448445a3489a7b9999485b86e79963b4f
eduardoneira <edun327@gmail.com>
2015-11-18 16:33:05 -0300

    para docker

---
+++ server_install.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+echo "Comenzando instalación Macho-Drive"
+cd /home/user/Macho-Drive/
+
+if [[ $(ls server/lib/RocksDB/ | grep "librocksdb.a") == "" ]] 
+then
+	echo "No se encontro rocksdb, instalando rocksdb"
+	./unpack_rocksdb;
+	echo "Rocksdb de ha instalado"
+fi
+
+cd server/
+echo "Comenzando compilacíon Macho-drive"
+./build.sh
+
+cd build
+echo "Macho-Drive comenzando"
+./Server
